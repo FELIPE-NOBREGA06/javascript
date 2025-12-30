181756037d17ce6c683f35e89fa2003aa00272b6
Felipe Cleverson <felipecleverson1@gmail.com>
2025-12-30 16:48:17 -0400

    metodos publicos

--- logica_de_programacao/Poo/metodos_privados_publicos/metodos_publicos.js
+++ logica_de_programacao/Poo/metodos_privados_publicos/metodos_publicos.js
@@ -1,3 +1,36 @@
-/** @format */
+class ContaBancaria {
+  constructor(titular, saldoInicial) {
+    this.titular = titular;
+
+    this.saldo = saldoInicial;
+  }
+
+  depositar(valor) {
+    this.saldo += valor;
+
+    console.log(`Depósito de ${valor} realizado. Novo saldo: ${this.saldo}`);
+  }
+
+  sacar(valor) {
+    if (valor > this.saldo) {
+      console.log(`Saldo insuficiente para sacar `);
+    } else {
+      this.saldo -= valor;
+      console.log(`Saque de ${valor} realizado. Novo saldo: ${this.saldo}`);
+    }
+  }
+
+  vericarSaldo() {
+    console.log(`Saldo Atual: ${this.saldo}`);
+  }
+}
 
 const minhaConta = new ContaBancaria("João", 1000);
+
+minhaConta.vericarSaldo();
+
+minhaConta.depositar(500);
+
+minhaConta.sacar(200);
+minhaConta.vericarSaldo();
+minhaConta.sacar(2000);
