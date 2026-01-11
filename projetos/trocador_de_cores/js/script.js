29188b45251a6ac726f6f4318e9c789c13ba16c7
CLEVERSON  SANTOS <152625169+FELIPE-NOBREGA06@users.noreply.github.com>
2026-01-11 00:49:00 -0400

    Projeto trocador_de_cores

---
+++ projetos/trocador_de_cores/js/script.js
@@ -0,0 +1,7 @@
+/** @format */
+
+document.getElementById("mudar-cor").addEventListener("click", function () {
+  const corAleatoria = Math.floor(Math.random() * 16777215).toString(16);
+
+  document.body.style.background = "#" + corAleatoria;
+});
